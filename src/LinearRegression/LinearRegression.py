4e053ce41d15001ce627e3458e6579dddb973f4f
Keisuke Hirota <pluvo2kei@gmail.com>
2023-12-16 21:17:20 +0900

    implemented LinearRegression itetation

--- src/LinearRegression/LinearRegression.py
+++ src/LinearRegression/LinearRegression.py
@@ -4,17 +4,26 @@ import itertools
 from sklearn.linear_model import LinearRegression
 
 
-def linear_regression(X: np.ndarray(), y: np.ndarray()):
+def fit_linear_regression(X: np.ndarray(), y: np.ndarray()):
     
     reg = LinearRegression().fit(X, y)
-    score = reg.score(X, y)
+    r2score = reg.score(X, y)
     
-    return reg, score
+    return reg, r2score
 
 
-def optimize_feature_set(input_data: pd.DataFrame(), label_data: pd.DataFrame(), feature_num: int):
+def iterate_feature_set(input_data: pd.DataFrame(), label_data: pd.DataFrame(), feature_num: int):
     
-    for feature_set in itertools.combinations(input_data.columns):
-        print(feature_set)
+    log_iteration = []
     
-    return None
+    for label in label_data.columns:
+        print(label)
+        
+        for feature_set in itertools.combinations(input_data.columns, feature_num):
+            print(feature_set)
+            input_data_selected = input_data[feature_set]
+            
+            reg, r2score = fit_linear_regression(input_data_selected.values, label_data[label].values)
+            log_iteration.append(feature_set + [label, r2score])
+    
+    return pd.DataFrame(log_iteration, columns=([f'feature_{i+1}' for i in range(feature_num)] + ['label', 'R2score']))
