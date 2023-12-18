816e79c74d97df0794ed782974feca9cd305ffb9
Keisuke Hirota <pluvo2kei@gmail.com>
2023-12-18 10:44:22 +0900

    implemented feature set optimization

--- src/LinearRegression/LinearRegression.py
+++ src/LinearRegression/LinearRegression.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import itertools
 from sklearn.linear_model import LinearRegression
+import pickle
 
 
 def fit_linear_regression(X: np.ndarray(), y: np.ndarray()):
@@ -27,3 +28,20 @@ def iterate_feature_set(input_data: pd.DataFrame(), label_data: pd.DataFrame(), 
             log_iteration.append(feature_set + [label, r2score])
     
     return pd.DataFrame(log_iteration, columns=([f'feature_{i+1}' for i in range(feature_num)] + ['label', 'R2score']))
+
+
+def optimize_feature_set(input_data: pd.DataFrame(), label_data: pd.DataFrame(), feature_num: int, output_dir_path: str):
+    
+    log_iteration = iterate_feature_set(input_data, label_data, feature_num)
+    log_iteration.to_csv(f'{output_dir_path}/log_iteration.csv', header=True, index=False)
+    
+    for label in label_data.columns:
+        log_iteration_extracted = log_iteration.query(f'label == "{label}"')
+        log_iteration_extracted.sort_values('R2score', ascending=False, inplace=False)
+        best_feature_set = log_iteration_extracted.iloc[1, :feature_num].to_list()
+        
+        best_reg, best_r2score = fit_linear_regression(input_data[best_feature_set].values, label_data[label].values)
+        model_path = f'{output_dir_path}/model/best_model_{label}.pkl'
+        pickle.dump(best_reg, model_path)
+    
+    return None
